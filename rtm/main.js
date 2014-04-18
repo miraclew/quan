7ed4002d44f3df7e2502b787114b7bb467759c28
miraclew <miraclew@gmail.com>
2014-04-18 16:17:46 +0800

    add websocket server code

---
+++ rtm/main.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    console.log('origin:' + origin); // e.g. http://www.baidu.com
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+function authToken(token) {
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    var token = request.resourceURL.query.token;
+    console.log('token:'+token);
+
+    if (!authToken(token)) {
+        request.reject();
+        console.log((new Date()) + ' Connection with token ' + token + ' rejected.');
+        return;
+    };
+
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept('mp-protocol-v1', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
