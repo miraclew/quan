e8fa93e2b4f3a3646391450860687783ad5c51bd
miraclew <miraclew@gmail.com>
2014-05-01 11:24:57 +0800

    aa

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -1,10 +1,42 @@
 <?php
 class CommentController extends BaseController {
-    public function postCreate()
-    {
+    public function index() {
+        $post_id = Input::get('post_id');
+        $limit = intval(Input::get('limit', 20));
+        $skip = intval(Input::get('skip', 0));
+
+        $query = DB::table('comments')
+            ->leftJoin('users', 'comments.user_id','=','users.id')
+            ->select('comments.*', 'users.nickname','users.avatar')
+            ->where('post_id','=',$post_id)
+            ->skip($skip)->take($limit);
+
+        $comments = $query->get();
+
+        return JR::ok(['objects' => $comments]);
     }
 
-    public function postDelete()
-    {
+    public function store() {
+        $post_id = Input::get('post_id');
+        $text = Input::get('text');
+        $user_id = Auth::user()->id;
+
+        $post = Post::find($post_id);
+        if (!$post) {
+            return JR::fail(Code::DATA_NOT_FOUND);
+        }
+
+        $is_member = Member::is_member($post->circle_id, $user_id);
+        if (!$is_member) {
+            return JR::fail(Code::NOT_ALLOW);
+        }
+
+        $comment = new Comment();
+        $comment->post_id = $post_id;
+        $comment->text = $text;
+        $comment->user_id = $user_id;
+        $comment->save();
+
+        return JR::ok(['object'=>$comment->toArray()]);
     }
 }
--- app/models/Member.php
+++ app/models/Member.php
@@ -4,4 +4,8 @@ class Member extends Eloquent {
     const TYPE_MEMBER = 1;
     const TYPE_MODERATOR = 2;
     const TYPE_ADMIN = 3;
+
+    public static function is_member($circle_id, $user_id) {
+        return self::whereRaw('circle_id=? and user_id=?')->count() > 0;
+    }
 }
